298179a94573b6c471f937ca0afa32a5428184e2
JairAparecimilliam <jrabc1991@gmail.com>
2023-07-28 16:56:15 +0000

    box with javax

---
+++ boxNameDialog/NameDialog.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class NameDialog
+{
+public static void main(String[] args)
+{
+
+//pede pro usuario inserir seu nome
+String name = 
+      JOptionPane.showImputDialog("What's your name?");
+
+//cria a mensagem 
+String message=
+      String.format("Welcome, %s, to Java Programming!", name);
+
+//exibe a mensagem para cumpromentar o usuario pelo seu nome
+JOptionPane.showMessageDialog(null, message);
+}//fim de main
+}//termina NameDialog
